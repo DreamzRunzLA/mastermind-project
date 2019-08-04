61ce251074e13a94d50dce5204e3237210e7f9fd
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-08-04 14:02:54 -0600

    Finished mastermind project

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -6,4 +6,64 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(charArr)
+    charArr.all? {|char| POSSIBLE_PEGS.has_key?(char.upcase)}
+  end
+
+  def initialize(pegs)
+    if Code.valid_pegs?(pegs)
+      @pegs = pegs.join('').upcase.split('')
+    else
+      raise 'not valid pegs'
+    end
+  end
+
+  def pegs
+    @pegs
+  end
+
+  def self.random(len)
+    pegArr = Array.new(len) {POSSIBLE_PEGS.keys[rand(0..3)]}
+    return Code.new(pegArr)
+  end
+
+  def self.from_string(pegString)
+    return Code.new(pegString.split(''))
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(codeInstance)
+    matches = 0
+    codeInstance.pegs.each_with_index do |peg, i|
+      if peg == @pegs[i]
+        matches += 1
+      end
+    end
+    return matches
+  end
+
+  def num_near_matches(codeInstance)
+    count = 0
+    codeInstance.pegs.each_with_index do |char, i|
+      if @pegs.include?(char) && codeInstance.pegs[i] != @pegs[i]
+        count += 1
+      end
+    end
+    return count
+  end
+
+  def ==(codeInstance)
+    if @pegs.join('') == codeInstance.pegs.join('')
+      true
+    else
+      false
+    end
+  end
 end
--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(len)
+        @secret_code = Code.random(len)
+    end
 
+    def print_matches(codeInstance)
+        puts @secret_code.num_exact_matches(codeInstance)
+        puts @secret_code.num_near_matches(codeInstance)
+    end
+
+    def ask_user_for_guess
+        puts 'Enter a code'
+        input = gets.chomp
+        instance = Code.from_string(input)
+        self.print_matches(instance)
+        return @secret_code == instance
+    end
 end
